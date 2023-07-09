32f91b20c8ecd69883af1e8f6ea4796c67963dc4
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-07-09 21:57:47 +0100

    updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
+import { GoogleLogo } from "phosphor-react";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -15,11 +16,12 @@ const Login = () => {
     }
   };
   return (
-    <main className="font-caveat">
-      <form></form>
-      <button onClick={signInWithGoogle} className="text-xl md:text-2xl border border-black px-4 py-1 sm:px-6 sm:py-2 rounded-md">
-        Google
-      </button>
+    <main className="font-caveat min-h-[80vh]">
+      <div className="w-full h-[70vh] grid place-items-center">
+        <button onClick={signInWithGoogle} className="text-xl md:text-2xl border border-black px-4 py-1 sm:px-6 sm:py-2 rounded-md">
+          Sign in with Google
+        </button>
+      </div>
       {error && <p>{error}</p>}
     </main>
   );
